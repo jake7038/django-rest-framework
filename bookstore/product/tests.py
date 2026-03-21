9271a383830e1431cf3e0b216f2c73e961c7bc53
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2026-03-21 09:30:13 -0300

    Feat: Testes unitários para os modelor Order,Product e category

--- bookstore/product/tests.py
+++ bookstore/product/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from product.models import Product, Category
 
 # Create your tests here.
+
+class ProductModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            title="Eletrônicos",
+            slug="eletronicos"
+        )
+
+        
+        self.product = Product.objects.create(
+            title="Notebook",
+            description="Notebook potente",
+            price=5000,
+            active=True
+        )
+
+        self.product.category.add(self.category)
+
+    def test_product_creation(self):
+        self.assertEqual(self.product.title, "Notebook")
+        self.assertEqual(self.product.price, 5000)
+        self.assertTrue(self.product.active)
+
+    def test_product_category_relationship(self):
+        self.assertEqual(self.product.category.count(), 1)
+        self.assertEqual(self.product.category.first().title, "Eletrônicos")
+
+    def test_product_str(self):
+        self.assertEqual(str(self.product), self.product.title)
+
+
+class CategoryModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            title="Livros",
+            slug="livros"
+        )
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.title, "Livros")
+        self.assertEqual(self.category.slug, "livros")
+
+    def test_category_str(self):
+        self.assertEqual(str(self.category), "Livros")
