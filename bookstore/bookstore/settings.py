beea93b5278cb1d7022d8d5c9a392bb4b8880949
Rafael do Amaral Costa <104049906+jake7038@users.noreply.github.com>
2026-05-07 10:28:06 -0300

    Atualizar o settings.py

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
