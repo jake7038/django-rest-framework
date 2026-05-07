a8215ea4864b86f255c1c5dcc0df82735590654a
Rafael do Amaral Costa <104049906+jake7038@users.noreply.github.com>
2026-05-07 11:57:27 -0300

    Update start.sh

--- bookstore/start.sh
+++ bookstore/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-python manage.py migrate --noinput
+python manage.py migrate auth --noinput
+python manage.py migrate contenttypes --noinput
+python manage.py migrate admin --noinput
+python manage.py migrate sessions --noinput
+python manage.py migrate authtoken --noinput
+python manage.py migrate product --noinput
+python manage.py migrate order --noinput
 python manage.py collectstatic --noinput
 gunicorn bookstore.wsgi:application --bind 0.0.0.0:8000
